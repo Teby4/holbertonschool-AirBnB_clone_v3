4b80fc7291dbdb56cafdf524844ca61b209576c9
root <felipeoliveramantero@gmail.com>
2024-04-21 14:41:09 -0700

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 
 from models import storage 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Response, jsonify, Flask
 from os import getenv
 
 app = Flask(import_name=__name__)
@@ -13,6 +13,13 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close(error):
     storage.close()
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """Handler for 404 errors"""
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 
 
 if __name__ == '__main__':
